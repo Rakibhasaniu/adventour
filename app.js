00a1f628dd51061200a49ce83da53caa016ad33e
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-04-16 15:36:34 +0600

    implemented crud operation

---
+++ app.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'Failed',
+      message: 'Invalid',
+    });
+  }
+  res.status(200).json({
+    status: 'Success',
+    result: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}...`);
+});
